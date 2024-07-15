88fcb634b023cc8fa23d69ba73cebbe701fa876e
Saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-15 11:49:17 +0530

    cidr fix sg

--- lms_vpc.tf
+++ lms_vpc.tf
@@ -202,14 +202,14 @@ resource "aws_security_group" "ibm_web_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_web_sg_ssh" {
   security_group_id = aws_security_group.ibm_web_sg.id
-  cidr_ipv4         = 0.0.0.0/0
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
 }
 resource "aws_vpc_security_group_ingress_rule" "allow_web_sg_http" {
   security_group_id = aws_security_group.ibm_web_sg.id
-  cidr_ipv4         = 0.0.0.0/0
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
